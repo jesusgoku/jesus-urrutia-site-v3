a438740562bc06eed58b14f871727debee774ec5
Jesus Urrutia <jesus.urrutia@gmail.com>
2018-08-07 13:25:21 -0400

    First draft

---
+++ src/js/views/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Contact from '../components/Contact';
+
+import imageMe from '../../images/me-480.jpg';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-view">
+        <p className="home-view__image-wrapper"><img src={imageMe} className="home-view__image"/></p>
+        <h1 className="home-view__title">Jesús Urrutia</h1>
+        <h2 className="home-view__subtitle">Desarrollador</h2>
+        <Contact/>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const Encore = require('@symfony/webpack-encore');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+Encore
+  .setOutputPath('public/')
+  .setPublicPath('/')
+  .addEntry('app', './src/js/app.js')
+  .cleanupOutputBeforeBuild()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .enableReactPreset()
+  .enableSassLoader()
+  .addPlugin(new HtmlWebpackPlugin({
+    template: './src/index.html',
+    minify: true,
+  }))
+;
+
+module.exports = Encore.getWebpackConfig();
